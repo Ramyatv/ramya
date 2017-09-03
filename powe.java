ff7c7c7d6528dec86833bf95a2aed23af52bccf0
Ramyatv <ramyajothi66@gmail.com>
2017-09-03 16:30:12 +0100

    Create powe.java

---
+++ powe.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class powe
+{
+public static void main(String[] s)
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+double result=Math.pow(a,b);
+System.out.println(result);
+}
+}
