fc89089ea483aaf5ca7e03e0a24af2914f0da90a
Ramyatv <ramyajothi66@gmail.com>
2017-09-04 07:24:15 +0100

    Create day.java

---
+++ day.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class day
+{
+public static void main(String[] s)
+{
+Scanner sc=new Scanner(System.in);
+String a=sc.next();
+String b="sunday";
+if(a.equals(b))
+System.out.println("false");
+else
+System.out.println("true");
+}
+}
