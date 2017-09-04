8ff9a0b473e8ba7a1b0a106222fa4b7164dddd67
Ramyatv <ramyajothi66@gmail.com>
2017-09-04 07:17:15 +0100

    Create inte.java

---
+++ inte.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class inte
+{
+public static void main(String[] s)
+{
+Scanner sc=new Scanner(System.in);
+String a=sc.next();
+int b=Integer.parseInt(a);
+System.out.println(b);
+}
+}
